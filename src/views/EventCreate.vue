221309692ffa8567bc872fd6652c895048c8afcd
Omololu Emmannuel <emmaomoh@gmail.com>
2021-01-30 14:00:53 +0100

    Vuex MapState

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,14 +1,29 @@
 <template>
-  <h1>Create Event {{userName}}</h1>
+  <div>
+    <h1>Create Event {{`${userName} with ID ${userID}` }}</h1>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{cat}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import {mapState} from 'vuex';
+
   export default {
     name: 'EventCreate',
     computed: {
-      userName() {
-        return this.$store.state.user.name;
-      }
+      /*
+     ...mapState(['user','categories']); // No User Defined Names for Vuex State
+      */
+      ...mapState({
+        userName: state => state.user.name,
+        userID: state => state.user.id,
+        categories: 'categories'
+      }),
+
+
+
     },
   }
 </script>
