42165a4697b015e73fe82a714abfaaa0e51b7118
Omololu Emmannuel <emmaomoh@gmail.com>
2021-01-30 13:44:26 +0100

    Accessing Vuex State In Component

--- src/store.js
+++ src/store.js
@@ -4,7 +4,19 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 123,
+      name:'Omololu Emmanuel'
+    },
+    categories:[
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+    ]
+  },
   mutations: {},
   actions: {}
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Create Event</h1>
+  <h1>Create Event {{userName}}</h1>
 </template>
+
+<script>
+  export default {
+    name: 'EventCreate',
+    computed: {
+      userName() {
+        return this.$store.state.user.name;
+      }
+    },
+  }
+</script>
