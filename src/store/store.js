a37676c6d3c776d2649bfd39686dea2a2dc62f8a
Omololu Emmannuel <emmaomoh@gmail.com>
2021-02-26 08:21:21 +0100

    Create and Register Notification Module

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user' // Import VueX Module
 import * as event from './modules/event'
+import * as notification from './modules/notification'
 
 Vue.use(Vuex)
 
@@ -9,7 +10,8 @@ export default new Vuex.Store({
   // Register VueX Module
   modules: {
     user,
-    event
+    event,
+    notification
   },
   state: {
     categories: [
